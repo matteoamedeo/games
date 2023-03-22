32d579e78692ed1fba0bab2df1e351fcc5df4f4b
matteoamedeo <matteoamedeo@outlook.it>
2023-03-22 22:39:47 +0100

    commit 5

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const port = process.env.PORT || 3000;
 const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
